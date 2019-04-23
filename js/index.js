28e58d0a3c88579d92925fa4649a3e48934241a6
scx-web-48 <1317794151@qq.com>
2019-04-23 09:35:31 +0800

    浩云律所优势

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(function () {
     $('.btn').popover();
     $('.activeToggle').click(function(){
-        $(this).addClass('active').siblings().removeClass('active')
+        var srcStr = $(this).find('img').attr('src'); //选中元素的子元素img的src值
+        $(this).addClass('active').siblings().removeClass('active');
+        console.log(srcStr)
+        //$(this).find('img').attr('src', '111.png') //可以用这个方法设置img的src属性的值
     })
 })
