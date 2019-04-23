45aba852dfa57af7a4da27928ca8d4d16de029ab
Mac <han@MacdeMBP.lan>
2019-04-24 00:02:27 +0800

    律师介绍

--- js/index.js
+++ js/index.js
@@ -5,5 +5,24 @@ $(function () {
         $(this).addClass('active').siblings("li[data-select='select']").removeClass('active');
         console.log(srcStr)
         //$(this).find('img').attr('src', '111.png') //可以用这个方法设置img的src属性的值
-    })
+    });
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 5,
+        // spaceBetween: 20,
+        // loopAdditionalSlides : 2,
+        loopFillGroupWithBlank: true,
+        centeredSlides: true,
+        // loop: true,
+        slideToClickedSlide: false,
+        snapOnRelease: false,
+        navigation: {
+            nextEl: '.swiper-btn-next',
+            prevEl: '.swiper-btn-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            dragSize: 300,
+            draggable: true,
+        },
+      });
 })
